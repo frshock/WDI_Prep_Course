41c14998eea2729519aff474a085b0e87192da83
frshock <will.s.crane@gmail.com>
2016-09-16 11:43:33 -0400

    Challenge 5

---
+++ Challenge_5/challenge5.js
@@ -0,0 +1,35 @@
+//Exercise 1
+var name = "Will";
+console.log("Hello "+ name);
+
+//Exercise 2
+var x = 1125;
+var answer = x%2;
+console.log(answer);
+
+//Exercise 3 + 4
+var a = true;
+var b = false;
+var c = true;
+console.log(a || b || c);
+console.log(a && b && c);
+
+//Exercise 5
+var answer1 = 10-3*5/4+202%3;
+var answer2 = 10-(3*(5/4)+(202%3));
+console.log(answer1);
+console.log(answer2);
+
+//Exercise 6
+var int1 = 12;
+var str1 = int1.toString();
+
+var str2 = "113";
+var int2 = parseInt(str2);
+
+var str3 = "1.12356";
+var flo1 = parseFloat(str3);
+
+var str4 =  "Hello World";
+var z = parseInt(str4);
+console.log(z);
